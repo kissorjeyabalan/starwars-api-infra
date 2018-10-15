242713e0613c6d305b93fec4234a9ff528a15560
Kissor Jeyabalan <git@kij.no>
2018-10-15 15:08:00 +0200

    IaC

---
+++ terraform/heroku.tf
@@ -0,0 +1,55 @@
+#### CI
+resource "heroku_app" "ci" {
+  name = "${var.app_prefix}-ci"
+  region = "eu"
+}
+
+resource "heroku_addon" "db_ci" {
+  app = "${heroku_app.ci.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+#### Staging
+resource "heroku_app" "staging" {
+  name = "${var.app_prefix}-staging"
+  region = "eu"
+}
+
+resource "heroku_addon" "db_staging" {
+  app = "${heroku_app.staging.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+#### Production
+resource "heroku_app" "production" {
+  name = "${var.app_prefix}-prod"
+}
+
+resource "heroku_addon" "db_prod" {
+  app = "${heroku_app.production.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+### Add stages to pipeline
+resource "heroku_pipeline" "swapi" {
+  app = "${var.pipeline_name}"
+  region = "eu"
+}
+
+resource "heroku_pipeline_coupling" "ci" {
+  app = "${heroku_app.ci.name}"
+  pipeline = "${heroku_pipeline.swapi.id}"
+  stage = "development"
+}
+
+resources "heroku_pipeline_coupling" "staging" {
+  app = "${heroku_app.staging.name}"
+  pipeline = "${heroku_pipeline.swapi.id}"
+  stage = "staging"
+}
+
+resources "heroku_pipeline_coupling" "production" {
+  app = "${heroku_app.production.name}"
+  pipeline = "${heroku_pipeline.swapi.id}"
+  stage = "production"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "app_prefix" {
+    default = "swapi"
+}
+
+variable "pipeline_name" {
+    default = "swapi--pipeline"
+}
