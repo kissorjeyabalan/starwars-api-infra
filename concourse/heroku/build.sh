a07c182eb551dda407a5963a9f58bb14094e5344
Kissor Jeyabalan <git@kij.no>
2018-10-15 20:39:17 +0200

    Added dependency caching

---
+++ concourse/heroku/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -x
+
+export DIR=$( pwd )
+export REPO=sw-api
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Symlinking cache"
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd ${DIR}/${REPO}
+mvn clean install
+
