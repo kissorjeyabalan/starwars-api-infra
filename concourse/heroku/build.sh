97b55ed26811de0222f11ed9026bd274d06d13c4
Kissor Jeyabalan <git@kij.no>
2018-10-15 20:51:21 +0200

    testing filepath

--- concourse/heroku/build.sh
+++ concourse/heroku/build.sh
@@ -3,15 +3,14 @@ set -e
 set -x
 
 export DIR=$( pwd )
-export REPO=sw-api
 
 M2_HOME="${HOME}/.m2"
-M2_CACHE="${REPO}/maven"
+M2_CACHE="${DIR}/maven"
 
 echo "Symlinking cache"
 [[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
 
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd ${DIR}/${REPO}
+cd ${DIR}/source
 mvn clean install
 
