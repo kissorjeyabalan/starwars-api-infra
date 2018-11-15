ede116a32b89f91ae211961fde9e10fc8130bf0b
Kissor Jeyabalan <git@kij.no>
2018-11-15 12:33:37 +0100

    added some basic tasks

---
+++ concourse/app/build.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+set -ueox pipefail
+
+export GREEN='\033[1;32m'
+export NC='\033[0m'
+export CHECK="√"
+export M2_LOCAL_REPO=".m2"
+export DIR=$( pwd )
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${DIR}/maven"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo -e "${GREEN}${CHECK} Cache symlinked!${NC}"
+
+mvn -f source/pom.xml install
+echo -e "${GREEN}${CHECK} Artifact built successfully!${NC}"
