12bb6f77c785b1e4147425839f363b167cb3a0b7
Kissor Jeyabalan <git@kij.no>
2018-11-15 09:58:47 +0100

    added heroku tf

---
+++ terraform/heroku.tf
@@ -0,0 +1,60 @@
+##### CI
+resource "heroku_app" "ci" {
+  name = "${var.app_name}-ci"
+  region = "eu"
+}
+
+## CI DB
+resource "heroku_addon" "db_ci" {
+  app = "${heroku_app.ci.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+#### Staging
+resource "heroku_app" "staging" {
+  name = "${var.app_name}-staging"
+  region = "eu"
+}
+
+## Staging DB
+resource "heroku_addon" "db_staging" {
+  app = "${heroku_app.staging.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+#### Production
+resource "heroku_app" "production" {
+  name = "${var.app_name}-production"
+  region = "eu"
+}
+
+## Production DB
+resource "heroku_addon" "db_production" {
+  name = "${heroku_app.production.name}"
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+
+##### Pipelines
+resource "heroku_pipeline" "exam-app" {
+  name = "${var.pipeline_name}"
+}
+
+# Connect apps to different pipeline stages
+resource "heroku_pipeline_coupling" "ci" {
+  app = "${heroku_app.ci.name}"
+  pipeline = "${heroku_pipeline.exam-app.id}"
+  stage = "development"
+}
+
+resource "heroku_pipeline_coupling" "staging" {
+  app = "${heroku_app.staging.name}"
+  pipeline = "${heroku_pipeline.exam-app.id}"
+  stage = "staging"
+}
+
+resource "heroku_pipeline_coupling" "production" {
+  app = "${heroku_app.ci.production}"
+  pipeline = "${heroku_pipeline.exam-app.id}"
+  stage = "production"
+ }
