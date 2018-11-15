a1f120945c78318b132ca663cd9459504a7fe3a4
Kissor Jeyabalan <git@kij.no>
2018-11-15 14:16:02 +0100

    updated task

--- concourse/app/build.sh
+++ concourse/app/build.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 set -ueox pipefail
 
 export GREEN='\033[1;32m'
@@ -10,12 +10,10 @@ export DIR=$( pwd )
 M2_HOME="${HOME}/.m2"
 M2_CACHE="${DIR}/maven"
 
-ls -a .
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo -e "${GREEN}${CHECK} Cache symlinked!${NC}"
 
-#[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
-#SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-#echo -e "${GREEN}${CHECK} Cache symlinked!${NC}"
-
-#mvn -f source/pom.xml install
-#echo -e "${GREEN}${CHECK} Artifact built successfully!${NC}"
-#ls .
+mvn -f source/pom.xml install
+echo -e "${GREEN}${CHECK} Artifact built successfully!${NC}"
+find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
